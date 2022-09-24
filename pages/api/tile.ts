ae5c7b7d75016cc4165d719565e5dc345294176d
Stu <stuart.osborne@nordnet.se>
2022-09-24 14:08:39 +0200

    Removed unused code to make empty project.

--- pages/api/tile.ts
+++ pages/api/tile.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type Tile = {
+  id: number,
+  rotation: number
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Tile>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    id: 1,
+    rotation: 0
+  })
 }
