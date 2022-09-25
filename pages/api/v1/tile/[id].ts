d2e3d104a5a3dce6d341d6f4a7d54eba71f222f2
Stu <stuart.osborne@nordnet.se>
2022-09-25 14:14:56 +0200

    Get an object from the database.

---
+++ pages/api/v1/tile/[id].ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../../lib/prisma';
+
+type Tile = {
+  id: number,
+  rotation: number,
+  image: string,
+  description: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Tile>
+) {
+  const {id} = req.query;
+  const tileWithId = await prisma.tile.findUnique({
+    where: {
+      id: parseInt(id as string)
+    }
+  });
+  console.log(tileWithId)
+  return res.status(200).json(tileWithId as Tile);
+}
+
