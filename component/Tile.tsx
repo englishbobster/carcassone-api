660d5d856a21bf25fcb289a00180a60bf776afe0
Stu <stuart.osborne@nordnet.se>
2022-10-10 18:25:36 +0200

    Rotate Tile on click.

--- component/Tile.tsx
+++ component/Tile.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
-type TileProps ={
+type TileProps = {
   imageName: string
 }
 
-export const Tile = ({imageName}: TileProps) =>
-    <div>
-      <button>
-        <img src={imageName}/>
-      </button>
-    </div>
+export const Tile = (tileProps: TileProps) => {
+
+  const [rotation, setRotation] = useState(0)
+
+  function rotateTile() {
+    let currentRotation = rotation%360 + 90;
+    return () => setRotation(currentRotation);
+  }
+
+  return (
+      <div>
+        <button>
+          <img src={tileProps.imageName} style={{transform: `rotate(${ rotation }deg)` }} onClick={rotateTile()}/>
+        </button>
+      </div>
+  );
+}
