29d298b803b730368e591d13f5dffd4541b7dbb5
Stu <stuart.osborne@nordnet.se>
2022-10-11 18:44:58 +0200

    Migration to remove rotate column in tiles.

---
+++ prisma/migrations/20221011163428_remove_rotation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rotation` on the `tile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tile" DROP COLUMN "rotation";
